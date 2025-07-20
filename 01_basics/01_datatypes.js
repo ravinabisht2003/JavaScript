73a6e2ecf3ea54f1fce100b03fbd1765974d975b
Ravina Bisht <ravinabisht2003@gmail.com>
2025-07-20 16:51:42 +0000

    01 class - datatypes done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; 
+// treat all js code as newer version 
+
+// alert (3+3);
+// it's a node.js so we can't use alert
+
+let strname = "Ravina";
+let isboolean   = true;
+let num  = 123;
+
+// number = 2 to power 53 
+// bigint
+// String = "" or ''
+// boolean = true/false
+// null = standalone / object
+// undefine = empty 
+// symbol = unique
+
+console.log(num);
+console.log(typeof 12);
+console.log();
+console.log(strname);
+console.log(typeof "ravina");
+console.log();
+console.log(typeof null);
+
+
+// code => readable + future proof 
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 1232563;
+// only one method of const
+let   accountEmail = "ravinabisht2003@gmail.com";
+// within a scope
+var   accountPassword = "123456789";
+// outof scope and don't use that
+accountCity = "Jaiput";
+// don't use that
+let   accountState;
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+// accountId = 564566; // not allowed
+
+console.log(accountId);
