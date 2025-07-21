3579974a7579d79f0f71d17ee2a9070087965245
Ravina Bisht <ravinabisht2003@gmail.com>
2025-07-21 16:28:00 +0000

    conversion of data type

---
+++ 01_basics/01_conversionofdatatype.js
@@ -0,0 +1,47 @@
+let score = 33;
+
+// console.log(typeof (score));
+
+//  conversion of datatype in number.
+
+// let stringValue = "ravina";
+// let stringValue = "";
+// let stringValue = undefined;
+// let stringValue = null;
+// let stringValue = 9+5; 
+// let stringValue = 9+"abc";
+// let stringValue = true;
+let stringValue = 1 + true;
+let stringInNumber = Number(stringValue);
+// console.log(stringInNumber);
+
+// null => 0, undefined => NaN, "ravina"=> NaN, 9+5 => number, 9+"abc" => NaN, 9+5 => 14, true => 1, 1+true => 2
+
+// conversion of datatype in String
+
+
+// let numberValue = "";
+// let numberValue = undefined;
+// let numberValue = 9+5;
+// let numberValue = true;
+let numberValue = 1.2;
+let numberInString = String(numberValue);
+// console.log(numberInString);
+
+// null => empty, undefined => undefined , 9+5 => 14, true => true, 1.2 => 1.2
+
+// data type into Boolean
+// let value = 10;
+// let value="";
+let value = undefined;
+
+let valueInBoolean = Boolean(value);
+console.log(valueInBoolean);
+
+// "ravina" => true, 5 => true, "" => false, undefined => false
+
+
+
+
+
+
