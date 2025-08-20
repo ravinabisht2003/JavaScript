43bb4b59d71d659d49b5dde1bb63b3af3eb3860e
Ravina Bisht <ravinabisht2003@gmail.com>
2025-08-20 16:28:28 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Named IIFE
+(
+    function chai (name){
+
+        console.log(`Ravina and ${name}`);
+    }
+)('Ira');
+
+(
+    (name)=>{
+
+        console.log(`Ravina and ${name}`);
+    }
+)('Aarav');
+
